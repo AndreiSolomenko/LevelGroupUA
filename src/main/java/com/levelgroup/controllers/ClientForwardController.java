14b75e2146f07b326408ce7173cd198e8b43c278
AndreiSolomenko <andrsolo10@gmail.com>
2024-01-21 18:45:43 +0200

    submission form fixed

--- src/main/java/com/levelgroup/controllers/ClientForwardController.java
+++ src/main/java/com/levelgroup/controllers/ClientForwardController.java
@@ -1,14 +1,14 @@
-package com.levelgroup.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class ClientForwardController {
-
-//    @GetMapping(value = {"/{path:[^\\.]*}", "/{path:^(?!api$).*$}/**/{path:[^.]*}"})
-    @GetMapping(value = "/**/{path:[^\\.]*}")
-    public String forward() {
-        return "forward:/";
-    }
-}
+//package com.levelgroup.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class ClientForwardController {
+//
+////    @GetMapping(value = {"/{path:[^\\.]*}", "/{path:^(?!api$).*$}/**/{path:[^.]*}"})
+//    @GetMapping(value = "/**/{path:[^\\.]*}")
+//    public String forward() {
+//        return "forward:/";
+//    }
+//}
